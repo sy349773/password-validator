d21e1f254f4dd2448657b053e1151255923ad2c5
sr <sy349773@dal.ca>
2017-05-26 16:30:45 -0300

    1.a code for stage 2 and 5 different rules

--- app/src/main/java/com/example/sr/password_validator/MainActivity.java
+++ app/src/main/java/com/example/sr/password_validator/MainActivity.java
@@ -12,6 +12,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Checks the number of rules the password has passed
+     * @param str
+     * @return return the int with the number of rules the password has passed.
+     */
+    public int numberOfRulesPassed(String str){
+
+        int num=0;
+
+        if (NotPassword(str)){
+            num++;
+        }
+        if (passwordLength(str)){
+            num++;
+        }
+        if(containNumber(str)){
+            num++;
+        }
+        if(containSpecialChar(str)){
+            num++;
+        }
+        if(containUpperAndLower(str)){
+            num++;
+        }
+        return num;
+    }
+
+    /**
      * NotPassword : checks whether a string is = "password:
      * parameters: String str
      * returns true if string is not "password"
@@ -27,5 +54,92 @@ public class MainActivity extends AppCompatActivity {
      */
 
     protected boolean passwordLength(String str){  int lenght = 8; return str.length()>=lenght; }
+
+    /**
+     * containSpecialChar checks if a string contains any of the following char:  !"#$%'()*+
+     * @param str String
+     * @return true if special character found false if not found
+     */
+    public boolean containSpecialChar(String str) {
+
+        int count=0;
+
+        while(count<str.length()){
+
+            // we use ANSI numbers to check for special chars. 33 = ! and 43 = +, rest of chars are in middle.
+            if(str.charAt(count)>=33&&str.charAt(count)<=43){
+                return true;
+            }
+
+            count++;
+
+        }
+
+        return false;
+
+    }
+
+
+    /**
+     *  checks if a string is composed of Upper and Lower case letters and return true if so
+     * @param str String
+     * @return
+     */
+    public boolean containUpperAndLower(String str) {
+
+        int count=0;
+        boolean containsUpper = false;
+        boolean containsLower = false;
+        boolean containsBoth = false;
+
+        while(count<str.length()&&!containsBoth){
+
+            // we use ANSI numbers to check for special chars.
+            // checks for upper case
+
+            if(str.charAt(count)>='A'&&str.charAt(count)<='Z'){
+                containsUpper = true;
+            }
+
+            //Checks for lowerCase
+            if(str.charAt(count)>='a'&&str.charAt(count)<='z'){
+                containsLower = true;
+            }
+
+            containsBoth = (containsUpper&&containsLower);
+
+            count++;
+
+        }
+
+        return containsBoth;
+
+    }
+
+    /**
+     * Checks if the String contains numbers. Returns true if so
+     * @param str
+     * @return
+     */
+    public boolean containNumber(String str) {
+
+        int count=0;
+
+        while(count<str.length()){
+
+            // we use ANSI numbers to check for special chars.
+            if(str.charAt(count)>='0'&&str.charAt(count)<='9'){
+                return true;
+            }
+
+            count++;
+
+        }
+
+        return false;
+
+    }
+
+
 
 }
--- app/src/test/java/com/example/sr/password_validator/ExampleUnitTest.java
+++ app/src/test/java/com/example/sr/password_validator/ExampleUnitTest.java
@@ -13,6 +13,19 @@ public class ExampleUnitTest {
 
     private MainActivity main = new MainActivity();
 
+    @Test
+    public void numberOfRulesPassed(){
+        String str = "aAA#1ASDAS"; // 5 rules passed
+        String str2 = "aAA1ASDAS"; // 4 rules passed
+        String str3 = "aAAASDAS"; // 3 rules passed
+        String str4 = "aAA#"; // 3 rules passed
+        String str5 = "aAAA";// 2 rule passed
+        String str6 = "a"; //1 rules passed
+
+        assertTrue(main.numberOfRulesPassed(str)==5&&main.numberOfRulesPassed(str2)==4&&main.numberOfRulesPassed(str3)==3&&
+                main.numberOfRulesPassed(str4)==3&&main.numberOfRulesPassed(str5)==2&&main.numberOfRulesPassed(str6)==1);
+    }
+
     /**
      * Test to check whether a string is "password". Two different strings were provided
      */
@@ -37,6 +50,43 @@ public class ExampleUnitTest {
 
         assertTrue(main.passwordLength(str)&&main.passwordLength(str2)&&!main.passwordLength(str3));
 
+    }
+
+    /**
+     * Test to check containsSpecialChar
+     * @throws Exception
+     */
+    @Test
+    public void containSpecialChar() throws Exception {
+        String str = "asdfghjkl"; // false
+        String str2 ="asdfghj!"; // true
+        String str3 ="#"; // true
+        assertTrue(!main.containSpecialChar(str)&&main.containSpecialChar(str2)&&main.containSpecialChar(str3));
+    }
+
+    /**
+     * Checks if containUpperAndLower method works
+     * @throws Exception
+     */
+    @Test
+    public void containUpperAndLower() throws Exception {
+        String str= "aqwer"; // false
+        String str2="ABCD"; // false
+        String str3="asvBCD"; // true
+
+        assertTrue(!main.containUpperAndLower(str)&&!main.containUpperAndLower(str2)&&main.containUpperAndLower(str3));
+    }
+
+    /**
+     * checks if containsNumber method works
+     * @throws Exception
+     */
+    @Test
+    public void containsNumber() throws Exception{
+        String str = "asds#AA$%"; // false
+        String str2 ="aasa0asa"; // true
+        String str3 ="3"; // true
+        assertTrue(!main.containNumber(str)&&main.containNumber(str3));
     }
 
 }
